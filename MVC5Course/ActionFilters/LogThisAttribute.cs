a16a9390516a7b05a38afe5cb433e2593babe467
Rocky.Li <ak47588168@gmail.com>
2017-12-16 13:54:29 +0800

    22 練習 Action Filter 的設計方式

---
+++ MVC5Course/ActionFilters/LogThisAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.ActionFilters
+{
+    public class LogThisAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // filterContext.Controller.ViewBag.Message = "Your application description page.";
+            // 
+            // do something
+            // 
+            // 
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
